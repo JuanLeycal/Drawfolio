3c650e59f2ee80a8a9039255ee5266e31e2a5f91
JuanLeycal <leyva.calle1403@gmail.com>
2021-03-06 18:41:28 -0500

    WIP🎨

---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+<v-card>
+  <div>
+      <h2>Drawfolio</h2>
+  </div>
+  <div>
+    <img src="src\assets\wip.png">
+  </div>
+</v-card>
+</template>
+
+<script>
+export default {
+  name: 'test'
+
+}
+</script>
+
+
+<style scoped>
+h2{
+  font-family: 'Gloria Hallelujah', cursive;
+  font-size: 4rem;
+}
+
+</style>
